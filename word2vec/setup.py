666d22394822b8d13e90e8981bb127af27d2e665
Quynh Nguyen <qmn203@nyu.edu>
2021-01-05 22:47:30 -0500

    chore: add setup.py

---
+++ word2vec/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='detm-arxiv',
+      version='0.1',
+      description='tools to get word embedding from arxiv.org abstracts',
+      url='https://github.com/quynhneo/detm-arxiv',
+      author='Quynh M. Nguyen',
+      author_email='qmn203@nyu.edu',
+      license='MIT',
+      packages=[word2vec],
+      zip_safe=False)
