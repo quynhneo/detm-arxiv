37f8b70357d7aa43b3100d7747fec551da503139
Quynh Nguyen <qmn203@nyu.edu>
2021-01-05 23:16:12 -0500

    chore: rename package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='detmarxiv',
+setup(name='arxivtools',
       version='0.1',
       description='tools to get word embedding from arxiv.org abstracts',
       url='https://github.com/quynhneo/detm-arxiv',
       author='Quynh M. Nguyen',
       author_email='qmn203@nyu.edu',
       license='MIT',
-      packages=['word2vec'],
+      packages=['arxivtools'],
       zip_safe=False)
